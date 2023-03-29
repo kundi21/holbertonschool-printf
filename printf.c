132bb8cbd485a782511070aaa110a509e27c2c37
juandi131 <juandigarcia3@gmail.com>
2023-03-29 13:08:41 -0700

    printf.

--- printf.c
+++ printf.c
@@ -7,9 +7,12 @@
 int _printf(const char *format, ...)
 {
 	char *buffer
-	
+	char *string
+
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format == %)
-		{
-			
+		{ 
+		string[i] = format[i] 
+		string[i + 1] =	format [i + 1]
+		}	 
