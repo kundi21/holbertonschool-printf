f2f747e4f409f539482edecf744ceb80c24740e5
juandi131 <juandigarcia3@gmail.com>
2023-03-29 11:45:20 -0700

    printf

--- printf.c
+++ printf.c
@@ -1,2 +1,15 @@
 #include "printf.h"
-#include " 
+#include 
+/**
+ *
+ *
+ */
+int _printf(const char *format, ...)
+{
+	char *buffer
+	
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format == %)
+		{
+			
