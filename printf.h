b836d48e8b717276363ca005faf4e0648575966e
kundi21 <cundi.machado@gmail.com>
2023-03-30 10:49:38 -0700

    asdasda

--- printf.h
+++ printf.h
@@ -7,16 +7,6 @@
 /**
  *
  */
-typedef struct func_printf {
-	char *unsigned_character
-	char *string
-	float *digit
-	int *integer
-
-	
-	
-} _printf_t;
-
 int (*get_function_printf_func(char *s));
 
 int _printf(const char *format, ...);
--- printf1.c
+++ printf1.c
@@ -5,30 +5,28 @@
  */
 int _printf(const char *format, ...)
 {
-	char *buffer
-	char *string
 	int i,n;
 	va_list arg;
 
 	va_start(arg, format);
 
-	va_end(arg);
 	
 	if (format == NULL)
 	{
 		return (-1)
 	}
+
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format == %)
-		{ 
-			string[i] = format[i]
-				if (
-					string[i + 1] =	format [i + 1]
+		if (format[i] == '%')
+		{
+			//function = get_op_func formst{i + 1}
+		}
+		else{
 		}
 	}
-	for (n = 0; format[n] != '\0'; n++)
-		write (1, format, i)
+
+	va_end(arg);
 }
 	
 
