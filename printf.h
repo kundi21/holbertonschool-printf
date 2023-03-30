7e3a6961712b68f9e67f6ce48b7976af1df4509b
kundi21 <cundi.machado@gmail.com>
2023-03-30 10:55:03 -0700

    update 15:00

--- printf.h
+++ printf.h
@@ -5,8 +5,19 @@
 #include <stdio.h>
 
 /**
+ * struct op - Struct op
  *
+ * @op: The operator
+ * @f: The function associated
  */
+typedef struct op
+{
+    char op;
+    int (*f)();
+} op_t;
+
+int (*get_op_func(char *s));
+
 int (*get_function_printf_func(char *s));
 
 int _printf(const char *format, ...);
