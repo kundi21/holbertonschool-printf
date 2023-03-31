bf2a457a8260daa8873c0c67bdd117beab600500
juandi131 <juandigarcia3@gmail.com>
2023-03-31 06:13:22 -0700

    printf.h

---
+++ _printf1.c
@@ -0,0 +1,39 @@
+#include "printf.h" 
+/**
+ * _printf - -
+ * @format
+ * Return: -
+ */
+int _printf(const char *format, ...)
+{
+	char k;
+	int i, n;
+	va_list arg;
+
+	va_start(arg, format);
+
+	
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			k = format(i + 1);
+			get_op_func(k, arg);
+		}
+		else
+		{
+			_putchar(format[i]);
+		}
+	}
+	
+	return (0);
+
+	va_end(arg);
+}
+	
+
