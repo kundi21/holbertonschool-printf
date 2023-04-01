888817f0f16e92497584c4cda598c33a16474d26
kundi21 <cundi.machado@gmail.com>
2023-04-01 15:15:57 -0700

    betty arreglado

--- get_op_func.c
+++ get_op_func.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
+ * get_op_func - get function
+ * @s: s
+ * Return: int.
  */
-int (* get_op_func(char s))(va_list)
+int (*get_op_func(char s))(va_list)
 {
-	op_t ops[] = 
-	{
+	op_t ops[] = {
 		{'c', printChar},
 		{'s', printString},
 		{'%', printPercentage},
@@ -12,14 +14,14 @@ int (* get_op_func(char s))(va_list)
 		{'i', printNum},
 		{'\0', NULL}
 	};
-	
+
 	int i;
 
 	for (i = 0; ops[i].op != '\0'; i++)
 	{
 		if (ops[i].op == s)
-			return ops[i].f;
+			return (ops[i].f);
 	}
-	
+
 	return (NULL);
-}	
+}
