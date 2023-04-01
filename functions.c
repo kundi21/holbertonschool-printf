79cb50a8f958d23170a4820b97d76cbf45b3fb28
kundi21 <cundi.machado@gmail.com>
2023-04-01 14:53:08 -0700

    betty arreglado

--- functions.c
+++ functions.c
@@ -1,17 +1,29 @@
 #include "main.h"
 /**
- *
+ * printChar - print char
+ * @args: va_list
+ * Return: int
  */
-
 int printChar(va_list args)
 {
 	return (_putchar(va_arg(args, int)));
 }
 
+/**
+ * printString - print string
+ * @args: va_list
+ * Return: int.
+ */
+
 int printString(va_list args)
 {
 	return (_putstrs(va_arg(args, char*)));
 }
+/**
+ * printNum - print num
+ * @args: va_list
+ * Return: int
+ */
 
 int printNum(va_list args)
 {
@@ -23,6 +35,12 @@ int printNum(va_list args)
 
 	return (_putstrs(str));
 }
+/**
+ * printPercentage - print %
+ * @args: va_list
+ * Return: int
+ */
+
 int printPercentage(__attribute__((unused)) va_list args)
 {
 	return (_putchar('%'));
