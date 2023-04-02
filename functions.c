5cec1dcdefce7f16ed93bda3f2daff0926045e8b
juandi131 <juandigarcia3@gmail.com>
2023-04-02 11:41:46 -0700

    1

--- functions.c
+++ functions.c
@@ -1,21 +1,27 @@
 #include "main.h"
 /**
  * printChar - This function print a char.
- * printString - This function print a String.
- * printNum - This function print a num.
- * printPercentage -  This function print a percent
+ * @args: This is a list
+ * Return: A value
  */
-
 int printChar(va_list args)
 {
 	return (_putchar(va_arg(args, int)));
 }
-
+/**
+ * printString - This function print a string.
+ * @args: List
+ * Return: A value.
+ */
 int printString(va_list args)
 {
 	return (_putstrs(va_arg(args, char*)));
 }
-
+/**
+ * printNum - This function print Num.
+ * @args: Print a num.
+ * Return: A value.
+ */
 int printNum(va_list args)
 {
 	long int n = va_arg(args, int);
@@ -26,8 +32,12 @@ int printNum(va_list args)
 
 	return (_putstrs(str));
 }
+/**
+ * printPercentage - This function print a percentaje.
+ * @args: This is a list.
+ * Return: A value
+ */
 int printPercentage(__attribute__((unused)) va_list args)
 {
 	return (_putchar('%'));
 }
-
