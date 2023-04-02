a5b8147baaa4779b916337557f9f003c6474170c
juandi131 <juandigarcia3@gmail.com>
2023-04-01 17:33:55 -0700

    1

--- _itoa1.c
+++ _itoa1.c
@@ -1,25 +1,32 @@
 #include "main.h"
 /**
- * reverse: this function reverse an a string.
+ * reverse - this function reverse an a string.
  * @str: This is a string.
  * @len: This is a integer.
  */
-void reverse(char *str, int len) {
-    int i = 0, j = len - 1;
-    while (i < j) {
-        char temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
-        i++;
-        j--;
-    }
+void reverse(char *str, int len)
+{
+	int i = 0, j = len - 1;
+	char temp;
+
+	while (i < j)
+	{
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+		i++;
+		j--;
+	}
 }
 /**
- * _itoa - This function 
- * @numb: This is a num
+ * _itoa - This is a function.
+ * @num: This is a num.
+ * @str: Is a string.
  */
-void _itoa(int num, char *str) {
+void _itoa(int num, char *str)
+{
 	int i = 0;
+
 	if (num < 0)
 	{
 		negativenumber(num, str);
@@ -33,7 +40,7 @@ void _itoa(int num, char *str) {
 	reverse(str, i);
 }
 /**
- * negativenumber: Use this code negative numbers.
+ * negativenumber - Use this code negative numbers.
  * @num: Is an in int.
  * @str: Is a str.
  */
@@ -42,7 +49,7 @@ void negativenumber(int num, char *str)
 	int a;
 
 	a = 0;
- 	do {
+	do {
 		str[a++] = ((num % 10) * -1) + '0';
 		num /= 10;
 	} while (num < 0);
